5a8aab97e70a6c430c9182c32e9eb23aa794a940
Biminli <mailofgod@126.com>
2017-10-23 23:50:07 +0800

    try add employee addresses query

--- src/asset/EmployeeList.js
+++ src/asset/EmployeeList.js
@@ -2,17 +2,73 @@
  * Created by biml on 2017/10/23.
  */
 import React, {Component} from "react";
-import {Layout, Menu, Breadcrumb, Icon, Row, Col} from 'antd';
+import {
+    Layout,
+    Menu,
+    Breadcrumb,
+    Icon,
+    Row,
+    Col,
+    Select,
+    DatePicker
+} from 'antd';
 import logo from './logo.png';
 import asset from './asset.svg';
 import './AssetApp.css';
 import {log} from './Config';
-import Login, {LoginOut} from './Login';
+import 'fetch-polyfill';
+import {log, urlBase} from './Config';
+import {utils} from './Utils';
 
 /**
  * 员工列表组件
  */
 class EmployeeList extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            officeAddresses:[]
+        };
+    }
+
+    componentDidMount(){
+        // get office Addresses
+        let officeAddresses = [];
+        fetch(urlBase + "/dict/listByTypeCode?typeCode=574304b1-b726-11e7-828b-0a0027000009", {
+            method: 'get',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        }).then((response)=>{
+            log("response:", response);
+            return response.json()
+        }).then((json)=>{
+            log("json:", json);
+            if ("success" === json.status) {
+                log("success:", values.userName);
+                json.list.forEach((value)=>{
+                    officeAddresses.push(value.name)
+                });
+            } else if('fail' === json.status){
+                log("fail:", json);
+                this.setState({
+                    failMsg: json.message
+                });
+            } else {
+                log("fail:", json);
+                this.setState({
+                    failMsg: `${json.status} ${json.message}`
+                });
+            }
+            this.setState({
+                officeAddresses: officeAddresses
+            });
+        }).catch((ex)=>{
+            log("failed:", ex);
+
+        });
+    }
     render(){
         return(
             <div>
@@ -20,19 +76,36 @@ class EmployeeList extends Component{
                     <Col>
                         <Form onSubmit={this.handleSubmit} className="login-form">
                             <FormItem>
-                                {getFieldDecorator('userName', {
-                                    rules: [{required: true, message: '请输入用户名!'}],
+                                {getFieldDecorator('name', {
+                                    rules: [{required: true, message: '请输入员工姓名!'}],
                                 })(
-                                    <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="用户名"
+                                    <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="员工姓名"
                                            ref={(input) => { this.textInput = input; }}/>
                                 )}
                             </FormItem>
                             <FormItem>
-                                {getFieldDecorator('password', {
-                                    rules: [{required: true, message: '请输入密码!'}],
+                                {getFieldDecorator('code', {
+                                    rules: [{required: true, message: '请输员工编号!'}],
                                 })(
-                                    <Input prefix={<Icon type="lock" style={{fontSize: 13}}/>} type="password"
-                                           placeholder="密码"/>
+                                    <Input prefix={<Icon type="user" style={{fontSize: 13}}/>} placeholder="员工编号"/>
+                                )}
+                            </FormItem>
+                            <FormItem>
+                                {getFieldDecorator('inductionDate', {
+                                    rules: [{required: true, message: '请选择入职日期!'}],
+                                })(
+                                    <DatePicker  prefix={<Icon type="user" style={{fontSize: 13}}/>} onChange={onChange}
+                                                 placeholder="入职日期" />
+                                )}
+                            </FormItem>
+                            <FormItem>
+                                {getFieldDecorator('officeAddress', {
+                                    rules: [{required: true, message: '请输员工编号!'}],
+                                })(
+                                    <Select defaultValue="lucy" style={{ width: 120 }} onChange={handleChange}>
+                                        {this.data.officeAdresses.map((value)=>
+                                            <Option value="jack">{value.name}</Option>)}
+                                    </Select>
                                 )}
                             </FormItem>
                             <FormItem>
